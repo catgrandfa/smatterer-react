a83163e028952c3c138c4ca0e5744e9b0561ac6b
catgrandfa <catgrandfa9898@outlook.com>
2023-04-06 21:48:02 +0800

    feat: 🚀 引入react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-
+import Root from '../router/root'
 // const root = document.getElementById('root');
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />
+    }
+])
+
 const root = document.querySelector('#root')
 
 if (root) {
-    createRoot(root).render(<App />)
+    createRoot(root).render(
+        <StrictMode>
+            <RouterProvider router={router} />
+            <App />
+        </StrictMode>
+    )
 }
